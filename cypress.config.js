b7f2ef7bc8e86b43dee5d97ab2d603c70b5ab77a
sergey s <som-ana@yandex.ru>
2023-11-08 17:54:15 +0300

    второе задание

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'tqfm7g',
   e2e: {
     baseUrl: "http://qamid.tmweb.ru/admin/",
     setupNodeEvents(on, config) {
